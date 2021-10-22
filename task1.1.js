5d90a43d73370ab28171973e36e11ed94281d272
DESKTOP-1LNU19H\test <you@example.com>
2021-10-22 15:06:58 +0300

    fix task 1

--- task1.1.js
+++ task1.1.js
@@ -1,7 +1,17 @@
-var result = document.getElementById('list').children
-result[0].parentNode.insertBefore(result[4], result[1]);
-result[0].parentNode.insertBefore(result[4], result[3]);
-var changedResult = document.getElementById('list')
-var output = changedResult.innerHTML
+let result = document.getElementById('list').children
+// result[0].parentNode.appendChild(result[2].parentNode.replaceChild(result[0], result[4]));
+// result[0].parentNode.appendChild(result[0]);
+// result[0].parentNode.appendChild(result[1]);
+// result[0].parentNode.appendChild(result[0]);
+// var changedResult = document.querySelector('#list')
+// var output = changedResult.innerHTML
+
+let output = [
+    result[0].innerHTML,
+    result[4].innerHTML,
+    result[1].innerHTML,
+    result[3].innerHTML,
+    result[2].innerHTML
+]
 
 console.log(output)
